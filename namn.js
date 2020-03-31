a05da952a9173824bdb3aee96258e12f2ae6362f
adiphussain2001 <46708024+adiphussain2001@users.noreply.github.com>
2020-03-31 23:55:00 +0200

    Add files via upload

---
+++ namn.js
@@ -0,0 +1,90 @@
+
+//
+function arabiskNamn () {  //Nu skapar jag en funtion
+  const killNamn = ["Ahmed 👦", "Mahmoud 👦", "Muhammed 👦"]; //Nu skapar jag mina arrays för killnamn och namnger dem till killNamn
+  const tjejNamn = ["Fatima 👧", "Fatma 👧", "Layla 👧"];//Nu skapar jag mina arrays för tjejnamn och namnger dem tilltjejNamn
+
+  var slumpNr = Math.floor(Math.random() * 3); //NU slumpar jag namnen 
+
+  alert(killNamn[slumpNr] + ", " + tjejNamn[slumpNr]); //Nu skapar jag en alert popup som kommer att visa det slumpade tjej och kill namnen
+}
+
+//
+function franskaNamn () {
+  const killNamn = ["Alexandre 👦", "Paul 👦", "Raphael 👦"];
+  const tjejNamn = ["Adrienne 👧", "Agnes 👧", "Amelie 👧"];
+
+  var slumpNr = Math.floor(Math.random() * 3);
+
+  alert(killNamn[slumpNr] + ", " + tjejNamn[slumpNr]);
+}
+
+//
+function mediterrianskNamn () {
+  const killNamn = ["Aapep 👦", "Abaris 👦", "Achilles 👦"];
+  const tjejNamn = ["Alala 👧", "Alecto 👧", "Amelie 👧"];
+
+  var slumpNr = Math.floor(Math.random() * 3);
+
+  alert(killNamn[slumpNr] + ", " + tjejNamn[slumpNr]);
+}
+
+//
+function hindiNamn () {
+  const killNamn = ["Amitabh 👦", "Arjit 👦", "Shahrhuk 👦"];
+  const tjejNamn = ["Amrita 👧", "Anaisha 👧", "Deepika 👧"];
+
+  var slumpNr = Math.floor(Math.random() * 3);
+
+  alert(killNamn[slumpNr] + ", " + tjejNamn[slumpNr]);
+}
+
+//
+function afrikaansNamn () {
+  const killNamn = ["Ade 👦", "Adonis 👦", "Ajani 👦"];
+  const tjejNamn = ["Aba 👧", "Abebi 👧", "Akpena 👧"];
+
+  var slumpNr = Math.floor(Math.random() * 3);
+
+  alert(killNamn[slumpNr] + ", " + tjejNamn[slumpNr]);
+}
+
+//
+function indianNamn () {
+  const killNamn = ["Yuma 👦", "Wuyi 👦", "Tse 👦"];
+  const tjejNamn = ["Zihna 👧", "Yamka 👧", "Wikimak 👧"];
+
+  var slumpNr = Math.floor(Math.random() * 3);
+
+  alert(killNamn[slumpNr] + ", " + tjejNamn[slumpNr]);
+}
+
+//
+function svenskaNamn () {
+  const killNamn = ["Erik 👦", "Pär 👦", "Kjell 👦"];
+  const tjejNamn = ["Anna 👧", "Josefin 👧", "Viktoria 👧"];
+
+  var slumpNr = Math.floor(Math.random() * 3);
+
+  alert(killNamn[slumpNr] + ", " + tjejNamn[slumpNr]);
+}
+
+//
+function engelskaNamn () {
+  const killNamn = ["Mark 👦", "Ben 👦", "Lorence 👦"];
+  const tjejNamn = ["Amanda 👧", "Whitney 👧", "Emily 👧"];
+
+  var slumpNr = Math.floor(Math.random() * 3);
+
+  alert(killNamn[slumpNr] + ", " + tjejNamn[slumpNr]);
+}
+
+//
+function kinesiskaNamn () {
+  const killNamn = ["Jet-Li 👦", "Yen 👦", "Wang Wei 👦"];
+  const tjejNamn = ["Chen 👧", "Jinjing 👧", "Fang 👧"];
+
+  var slumpNr = Math.floor(Math.random() * 3);
+
+  alert(killNamn[slumpNr] + ", " + tjejNamn[slumpNr]);
+}
